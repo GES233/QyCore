ac25a70f10dc14ba99205d6a9a0c18be0961699a
RoastChestnut <chestnut.roasted@outlook.com>
2025-07-11 20:06:42 +0800

    update runner spec

--- lib/qy_core/runner.ex
+++ lib/qy_core/runner.ex
@@ -3,11 +3,17 @@ defmodule QyCore.Runner do
   定义运行时实现的功能。
   """
 
+  alias QyCore.Param
   alias QyCore.Runner.CookingContext, as: Context
 
   @callback valid?(Context.t()) :: :ok | {:error, term()}
 
   @callback runnable?(Context.t()) :: :ok | {:error, term()}
+
+  @callback param_unwrap(Param.t(), unwrap_option :: :param | :data) :: Param.t() | any()
+
+  # TODO: ensure spec
+  @callback param_wrap(any() | Param.t(), extra :: any()) :: Param.t()
 
   @callback execute(Context.t()) :: Context.t() | {:error, term()}
 end
